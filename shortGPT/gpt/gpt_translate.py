fa20322012e397325cb69fc94b808c10a73fb171
Wang Xian <wangxhere@gmail.com>
2023-12-17 08:15:10 +0800

    separate translate template for Ollama

--- shortGPT/gpt/gpt_translate.py
+++ shortGPT/gpt/gpt_translate.py
@@ -1,7 +1,10 @@
 from shortGPT.gpt import gpt_utils
+import os
 
 def translateContent(content, language):
-    chat, system = gpt_utils.load_local_yaml_prompt('prompt_templates/translate_content.yaml')
+    chat, system = gpt_utils.load_local_yaml_prompt('prompt_templates/ollama_translate_content.yaml') \
+        if os.getenv("USE_OLLAMA", "1") == "1" \
+        else gpt_utils.load_local_yaml_prompt('prompt_templates/translate_content_gpt3.yaml')
     if language == "arabic":
         language =="arabic, and make the translated text two third of the length of the original."
     system = system.replace("<<LANGUAGE>>", language)
